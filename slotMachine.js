c7da564d09cf721fc957fcc7eb8bf02e0da39a1b
Cole Cianflone <colecianflone123@gmail.com>
2022-06-29 20:33:08 -0500

    Update slotMachine.js

--- slotMachine.js
+++ slotMachine.js
@@ -1,3 +1,22 @@
-const leftSlot = [];
-const middleSlot = [];
-const rightSlot = [];
+// Initial load of the site.
+load();
+
+/* Load function of site.
+ *
+ * Sets inital state of site elements.
+ */
+function load(){
+
+    // Create array of images that will be randomized.
+    var images = 
+    ["images/1.png",
+    "images/2.png",
+    "images/3.png",
+    "images/4.png",
+    "images/5.png",
+    "images/6.png",
+    "images/7.png"];
+    
+    console.log(images.length);
+}
+
